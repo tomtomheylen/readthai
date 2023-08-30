923d2ddeb3e3af54f97ed6f202a5c2f9cf897e78
tomtomheylen <tomtomheylen@gmail.com>
2023-08-30 21:44:12 +0700

    u

---
+++ functionCalls.js
@@ -0,0 +1,6 @@
+ createElements();
+makeFirstLetterBold();
+createVowelElements();
+createLearningElements();
+togglePages();//this makes page2 appear on page load. comment to make pag1 appear first
+createMarkElements();
